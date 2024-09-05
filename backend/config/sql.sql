8114f7bfbd3d52c618779dd3ae5543adcabfc4cb
yikiat97 <limyikiat97@hotmail.com>
2024-09-05 19:50:18 +0800

    sql

--- backend/config/sql.sql
+++ backend/config/sql.sql
@@ -13,10 +13,14 @@ CREATE TABLE IF NOT EXISTS `accounts` (
  `id` int(11) NOT NULL,
  `username` varchar(50) NOT NULL,
  `password` varchar(255) NOT NULL,
- `email` varchar(100) NOT NULL
+ `email` varchar(100) ,
+ `accountStatus` varchar(30) NOT NULL,
+ `createDate` int NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-INSERT INTO `accounts` (`id`, `username`, `password`, `email`) 
-VALUES (1, 'yikiat', '$2b$10$QHWQvUhmxoCTqsLivcuE.OcE227AEG03Eb70lOeeNlnCxdvb4KDxe', 'yikiat@hotmail.com');
+INSERT INTO `accounts` (`id`, `username`, `password`, `email`,`accountStatus`,`createDate`) 
+VALUES (1, 'yikiat1', '$2b$10$QHWQvUhmxoCTqsLivcuE.OcE227AEG03Eb70lOeeNlnCxdvb4KDxe', 'yikiat@hotmail.com', 'active', 123);
+INSERT INTO `accounts` (`id`, `username`, `password`, `email`,`accountStatus`,`createDate`) 
+VALUES (2, 'yikiat2', '$2b$10$QHWQvUhmxoCTqsLivcuE.OcE227AEG03Eb70lOeeNlnCxdvb4KDxe', 'yikiat@hotmail.com', 'active', 123);
 ALTER TABLE `accounts` ADD PRIMARY KEY (`id`);
 ALTER TABLE `accounts` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 ALTER TABLE `accounts` ADD UNIQUE (`username`);
@@ -25,15 +29,15 @@ ALTER TABLE `accounts` ADD UNIQUE (`username`);
 -- Create the user_group table
 DROP TABLE IF EXISTS `user_group`;
 CREATE TABLE IF NOT EXISTS `user_group` (
-    `id` int(11) NOT NULL,
     `username` varchar(50) NOT NULL,
     `usergroup` varchar(50) NOT NULL,
-    CONSTRAINT `fk_username` FOREIGN KEY (`username`) REFERENCES `accounts` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
+    `createDate` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-INSERT INTO `user_group` (`id`, `username`, `usergroup`) 
-VALUES (1, 'yikiat', 'admin');
-ALTER TABLE `user_group` ADD PRIMARY KEY (`id`);
-ALTER TABLE `user_group` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+INSERT INTO `user_group` ( `username`, `usergroup`, `createDate`) 
+VALUES ('yikiat1', 'users',123);
+INSERT INTO `user_group` ( `username`, `usergroup`, `createDate`) 
+VALUES ('yikiat2', 'admin',123);
+
 
 -- Select all data from the accounts table
 SELECT * FROM accounts;
